cb74e0d493bc8342ebbea9c84cde93bb7b743421
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2024-12-02 13:43:40 -0500

    adds landing page with specific brand colors (#7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,118 @@
 // Next.js metadata types
 import type { Metadata } from "next";
 
-// API functions
-import { getAllArticles } from "@/lib/api";
-
-// Components
-import { ArticlesList } from "@/components/ArticlesList";
+// Icons
+import { ArrowRight } from "lucide-react";
 
 /**
  * Metadata configuration for SEO
  */
 export const metadata: Metadata = {
-  title: "Latest Articles",
-  description: "Browse our latest articles and blog posts",
+  title: "Contentful Next.js Starter",
+  description: "Contentful Next.js Starter",
 };
 
 /**
- * Homepage component displaying latest articles
+ * Landing page
  */
 export default async function HomePage() {
-  // Fetch initial articles for server-side rendering (3 latest articles)
-  const initialArticles = await getAllArticles();
-  console.log("Home page data:", initialArticles);
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-900 to-slate-800 text-white">
+    <div className="flex min-h-[calc(100vh-64px)] flex-col items-center justify-center bg-gradient-to-b from-slate-900 to-slate-800 text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Latest Articles
+        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <div className="flex gap-4">
+            <span className="text-gradient-pink">A</span>
+            {/* <span className="text-gradient-pink">|||</span> */}
+            <span className="text-gray-100">Matic Digital</span>
+            <span className="text-gradient-pink">Starter</span>
+          </div>
         </h1>
-        <ArticlesList
-          initialArticles={initialArticles.items}
-          initialTotal={initialArticles.total}
-          showPagination={false}
-        />
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
+          <a
+            href="https://app.contentful.com/sign-up/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-orange-500 p-4 text-white hover:bg-orange-500/50"
+          >
+            <h3 className="text-2xl font-bold">Contentful →</h3>
+            <p className="text-lg">Headless CMS</p>
+          </a>
+
+          <a
+            href="https://github.com/withgeist/nextjs-starter"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-black p-4 text-white hover:bg-black/40"
+          >
+            <h3 className="text-2xl font-bold">Next.js →</h3>
+            <p className="text-lg">React framework for the web</p>
+          </a>
+          <a
+            href="https://www.typescriptlang.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-blue-500 p-4 text-white hover:bg-blue-500/50"
+          >
+            <h3 className="text-2xl font-bold">TypeScript →</h3>
+            <p className="text-lg">Typed JavaScript</p>
+          </a>
+          <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-sky-400 p-4 text-white hover:bg-sky-400/50"
+          >
+            <h3 className="text-2xl font-bold">Tailwind CSS →</h3>
+            <p className="text-lg">Utility-first CSS</p>
+          </a>
+
+          <a
+            href="https://ui.shadcn.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-black p-4 text-white hover:bg-black/50"
+          >
+            <h3 className="text-2xl font-bold">Shadcn UI →</h3>
+            <p className="text-lg">Radix Primitives and Tailwind</p>
+          </a>
+          <a
+            href="https://mux.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-pink-500 p-4 text-white hover:bg-pink-500/50"
+          >
+            <h3 className="text-2xl font-bold">Mux →</h3>
+            <p className="text-lg">Video APIs, data and players</p>
+          </a>
+          <a
+            href="https://jotai.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white p-4 text-black hover:bg-white/50"
+          >
+            <h3 className="text-2xl font-bold">Jotai →</h3>
+            <p className="text-lg">Global state management</p>
+          </a>
+          <a
+            href="https://tanstack.com/query/v4/docs/overview"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-red-500 p-4 text-white hover:bg-red-500/50"
+          >
+            <h3 className="text-2xl font-bold">Tanstack Query →</h3>
+            <p className="text-lg">Data fetching and caching</p>
+          </a>
+          <a
+            href="https://tanstack.com/query/v4/docs/react-query-overview"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex max-w-xs flex-col gap-4 rounded-xl bg-amber-500 p-4 text-white hover:bg-amber-500/50"
+          >
+            <h3 className="text-2xl font-bold">Tanstack Form →</h3>
+            <p className="text-lg">Form management and validation</p>
+          </a>
+        </div>
       </div>
-    </main>
+    </div>
   );
 }
