5100b359077752b9cfb5dee92e957d6a7ce27f56
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2024-12-04 12:36:14 -0600

    file naming linting improvements

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,78 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": true
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
   },
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "extends": [
+  plugins: ["@typescript-eslint", "@eslint-react"],
+  extends: [
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked"
+    "plugin:@typescript-eslint/stylistic-type-checked",
   ],
-  "rules": {
+  rules: {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
+        prefer: "type-imports",
+        fixStyle: "inline-type-imports",
+      },
     ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
-        "argsIgnorePattern": "^_"
-      }
+        argsIgnorePattern: "^_",
+      },
     ],
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
-        "checksVoidReturn": {
-          "attributes": false
-        }
-      }
-    ]
-  }
-}
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
+  overrides: [
+    // https://eslint-react.xyz/docs/installation
+    // directories in components
+    {
+      files: ["src/components/**"],
+      rules: {
+        "@eslint-react/naming-convention/filename": ["error", "kebab-case"],
+      },
+    },
+    // custom components
+    {
+      files: ["src/components/**/*.{ts,tsx}"],
+      rules: {
+        "@eslint-react/naming-convention/filename": ["error", "PascalCase"],
+      },
+    },
+    // shadcn components installed via CLI
+    {
+      files: ["src/components/ui/*.{ts,tsx}"],
+      rules: {
+        "@eslint-react/naming-convention/filename": ["error", "kebab-case"],
+      },
+    },
+    // hooks
+    {
+      files: ["src/hooks/**/use*.{ts,tsx}"],
+      rules: {
+        "@eslint-react/naming-convention/filename": ["error", "camelCase"],
+      },
+    },
+    // shadcn hooks
+    {
+      files: ["src/hooks/**/use-*.{ts,tsx}"],
+      rules: {
+        "@eslint-react/naming-convention/filename": ["error", "kebab-case"],
+      },
+    },
+  ],
+};
 module.exports = config;
