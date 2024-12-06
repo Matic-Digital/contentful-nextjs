691fdaaa65f4bf09ddfc188655941b045b719967
stilianb <stilianb@gmail.com>
2024-12-06 11:56:31 -0700

    removed unused constants from toast.ts

--- src/constants/toast.ts
+++ src/constants/toast.ts
@@ -4,7 +4,3 @@
 
 export const TOAST_LIMIT = 1;
 export const TOAST_REMOVE_DELAY = 1000000;
-export const TOAST_TYPE_SUCCESS = 'success';
-export const TOAST_TYPE_ERROR = 'error';
-export const TOAST_TYPE_WARNING = 'warning';
-export const TOAST_TYPE_INFO = 'info';
