3c8b4b23a05a42bff791ad8bd05c9f2169c6a7b8
stilianb <stilianb@gmail.com>
2024-12-12 11:35:36 -0700

    updating prettier config to match commerce

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,13 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
+const config = {
+  semi: true,
+  singleQuote: true,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  bracketSpacing: true,
+  plugins: ['prettier-plugin-tailwindcss']
 };
+
+export default config;
